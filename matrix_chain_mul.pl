f27bba9ced870a417d861cbbbed7948e877bb102
aashishgoyal <aashish.goyal01@gmail.com>
2014-06-05 22:53:10 +0530

    Update matrix_chain_mul.pl
    The file now acts as a script to transform test.pl to test_out.pl which generates excel code. Added some new functions, controlling script and reordered some functions.
    Needs to be documented more comprehensively.

--- matrix_chain_mul.pl
+++ matrix_chain_mul.pl
@@ -1,47 +1,17 @@
-	use strict;
-	use Spreadsheet::XLSX;
+use Data::Dumper;
+my $headers = << 'END_MESSAGE';
+
+
+	##### EXCEL WRITER PORTION STARTS #####
+
 	use Excel::Writer::XLSX;
-
-
- 	my $file = $ARGV[0]; # name of the input excel file to be given as argument
- 	my $input = $ARGV[1]; # number of matrices
-
- 	my $excel = Spreadsheet::XLSX -> new ($file); # opens the input file
-
-	if (!defined $excel) {
-		die $excel->error(), ".\n";
-	}
-
-	my $sheet = $excel -> worksheet(0); # opens the default worksheet
-	my $input_text = ($sheet->get_cell('1','0'))->value();
-	
-	my @dimensions; # stores the input dimensions 
-	for(my $i = 1; $i <= $input + 1; $i++){
-		my $cell = $sheet->get_cell(1,$i);
-		$dimensions[$i] = $cell->value();
-	}
-
-
-	my $workbook  = Excel::Writer::XLSX->new( $file );# creats a new excel file with name same as that of input file
+	my $file = "output.xlsx";
+	my $workbook = Excel::Writer::XLSX->new($file);
 	my $worksheet = $workbook->add_worksheet();
+END_MESSAGE
+my $subroutines = << 'END_SUB';
 
 
-	my $num = $input; #number of matrices
-	my $n = $num+1;
-	# assume that B2 to B($n+1) contains the dimensions of the matrices
-	
-	# restores the input data in the new file
-	$worksheet->write( 'A2', $input_text); 
-	for(my $i = 1; $i <= $num + 1; $i++) {
-		$worksheet->write_number( '1',$i, $dimensions[$i]);
-	}
-
-	# fills in 0 in the diagonal
-	for(my $i = 1; $i < $n; $i++) {
-	    $worksheet->write_number( $i+3, $i-1, 0 );
-	}
-
-	my $a = ord('A');#ascii representation of A. Needed to later switch from numbers to chars
 	sub convert{
 		my $a = ord('A');
 		my $num = (shift @_);
@@ -55,31 +25,172 @@
 		}
 		return $ans;
 	}
-
-	# generates and fills in formulae in the upper triangle 
-	# fills in the matrices whose product is considered in the lower triangle
-	for(my $len=2; $len < $n; $len++){
-		for(my $i=1; $i < $n-$len+1; $i++){
-			my $j = $i+$len-1;
-			my $temp_formula = "=MIN(";
-			for(my $k = $i; $k <= $j-1; $k++){
-				$temp_formula = $temp_formula.convert($k-1).($i+4)."+".convert($j-1).($k+5)."+(".convert($i)."2*".convert($k+1)."2*".convert($j+1)."2),";
-			}
-			$temp_formula = substr($temp_formula, 0 ,-1);
-			$temp_formula = $temp_formula.")";
-			$worksheet->write_formula($i+3,$j-1,$temp_formula);
-
-			my $i1 = ($i + 3) - 4;
-			my $j1 = ($j - 1) - 0;
-			my $i2 = $j1 + 4;
-			my $j2 = $i1 + 0;
-			$worksheet->write($i2,$j2,"M$i..M".($i+$len-1));
+	sub get_index{#expects a ref to array names and a name whose index is to be found
+		my $input = shift (@_);my $name = shift (@_);my $i = 0;
+		while(defined $$input[$i][0]){
+			if($$input[$i][0] eq $name){return $i;}
+			$i = $i+1;
 		}
+		my $empty_val;
+		return $empty_val;#if no match return an empty variable;
 	}
-
-	# makes the cells square in shape
-	for (my $i = 0;$i < $num ;$i++){
-		$worksheet->set_row($i+4,40);
+	sub to_excel_pos{#expects a ref to array names and a string to excelify
+		my $input = (shift @_);my $string = shift @_;
+		while($string =~ m!\$(\w*)\[(\d+)\]\[(\d+)\]!g){
+			if (defined get_index($input,$1)){
+				my $index = get_index($input,$1);
+				my $temp = convert($$input[$index][2]+$3).($$input[$index][1]+$2+1);
+				$string =~ s!\$$1\[(\d+)\]\[(\d+)\]!$temp!;
+			}
+		}
+		while($string =~ m!\$(\w*)\[(\d+)\]!g){
+			if (defined get_index($input,$1)){
+				my $index = get_index($input,$1);
+				my $temp = convert($$input[$index][2]+$2).($$input[$index][1]+1);
+				$string =~ s!\$$1\[(\d+)\]!$temp!;
+			}
+		}
+		return $string;
 	}
+END_SUB
+	my $filename = 'test.pl';
+	my $filevar = "";
+	open(my $fh, '<:encoding(UTF-8)', $filename) or die "Could not open file '$filename' $!";
+	while (my $row = <$fh>){chomp $row;$filevar = $filevar.$row."\n";}
+	close $fh;
+	$input = find_array($filevar);
+	my $arrays = Dumper($input);
+	$filevar = comment_gobbler($filevar);
+	$filevar = create_bg_arrays($filevar);
+	$filevar = find_array_assignments($filevar);
+	$filevar = $filevar . $headers . $arrays . "\nmy \$arrays = \$VAR1;\n" . add_excel_funcs($input) . $subroutines;
+	$filevar =~ s/^\s+|\s+$//g;
 
-$workbook->close();
+	my $output_file = "test_out.pl";
+	open(my $f1h, '>', $output_file) or die "Could not open file '$output_file' $!";
+	print $f1h $filevar;
+	close $f1h;
+
+	sub find_array{
+		# 0th pos constains the name of array
+		# 1st pos contains the x pos(or row) of first cell
+		# 2nd pos contains the y pos(or col) of first cell
+		# 3rd pos contains the number of rows (undef for a 1-d array)
+		# 4th pos contains the number of columns
+		my $string = (shift @_);my @ans;my $i = 0;my @pos = (0,0);
+		while ($string =~ m![\n^][^\S\n\r]*my\s@(\w*);\s*#dim:(\w*)(\[(\d+)\]\[(\d+)\]|\[(\d+)\])!g ){
+			if ($1 eq $2){
+				$ans[$i][0] = $1; 
+				if (!(defined $4)){#if (!($4)){#$4 == null
+					#case of 1d array
+					$ans[$i][4] = $6; 
+					$ans[$i][1] = $pos[0]; $ans[$i][2] = $pos[1];
+					@pos = ($pos[0]+3,$pos[1]);
+				}
+				else{
+					#case of 2d array
+					$ans[$i][3] = $4;
+					$ans[$i][4] = $5;
+					$ans[$i][1] = $pos[0]; $ans[$i][2] = $pos[1];
+					@pos = ($pos[0]+$ans[$i][3]+2,$pos[1]);
+				}
+				$i = $i+1;
+			}
+		}
+		return \@ans;
+	}
+	sub find_array_assignments{
+		my $input = (shift @_);my @to_swap;my $i = 0;
+		while ($input =~ m!([\n\r^]([^\S\n\r]*)(\$(\w*)(\[[^\n\r\]]*\]|\[[^\n\r\]]*\]\[[^\n\r\]]*\]))[^\S\n\r]*=([^\n\r;]*);)!g){
+			$i = $i+1;
+			$to_swap[$i][0] = $1;
+			my $string = $1;
+			$string = $string . "\n$2\$bg_$4$5 = ";
+			my $temp = $6;
+			#swap for input var;
+			$temp =~ s!\$(\w*)\[!\\\$$1\[!g;
+			$to_swap[$i][1] = $string . "\"=" . $temp . "\";";
+		}
+		while($i>0){
+			$input =~ s!\Q$to_swap[$i][0]!$to_swap[$i][1]!;
+			$i = $i-1;
+		}
+		return $input;
+	}
+	sub add_excel_funcs{
+		my $input = (shift @_);
+		my $output = "";
+		for (my $j = 0;defined $$input[$j][0];$j++){
+			my $name = $$input[$j][0];
+			if(!(defined $$input[$j][3])){
+				$output = $output . "\n" .
+				"	for(my \$i = 0; \$i < \$\$arrays[$j][4];\$i++){" . 
+				"\n		\$bg_$name\[\$i\] = to_excel_pos(\$arrays,\$bg_$name\[\$i\]);".
+				"\n		\$worksheet->write($$input[$j][1], $$input[$j][2]+\$i, \$bg_$name\[\$i\]);".
+				"\n	}";
+			}
+			else{
+				$output = $output . "\n" .
+				"	for(my \$i = 0; \$i < \$\$arrays[$j][3];\$i++){"."\n".
+				"		for(my \$k = 0; \$k < \$\$arrays[$j][4];\$k++){" . 
+				"\n			\$bg_$name\[\$i\]\[\$k\] = to_excel_pos(\$arrays,\$bg_$name\[\$i\]\[\$k\]);".
+				"\n			\$worksheet->write($$input[$j][1]+\$i, $$input[$j][2]+\$k, \$bg_$name\[\$i\]\[\$k\]);".
+				"\n		}".
+				"\n	}";
+			}
+		}
+		return $output . "\n\$workbook->close();\n";
+	}
+	sub to_excel_pos{#expects a ref to array names and a string to excelify
+		my $input = (shift @_);my $string = shift @_;
+		while($string =~ m!\$(\w*)\[(\d+)\]\[(\d+)\]!g){
+			if (defined get_index($input,$1)){
+				my $index = get_index($input,$1);
+				my $temp = convert($$input[$index][2]+$3).($$input[$index][1]+$2+1);
+				$string =~ s!\$$1\[(\d+)\]\[(\d+)\]!$temp!;
+			}
+		}
+		while($string =~ m!\$(\w*)\[(\d+)\]!g){
+			if (defined get_index($input,$1)){
+				my $index = get_index($input,$1);
+				my $temp = convert($$input[$index][2]+$2).($$input[$index][1]+1);
+				$string =~ s!\$$1\[(\d+)\]!$temp!;
+			}
+		}
+		return $string;
+	}
+	sub create_bg_arrays{#expects a string to operate on
+		my $input = (shift @_);
+		$input =~ s![\n^]([^\S\n\r]*)my\s@(\w*);[^\S\n\r]*[\n\r]!\n$1my \@$2;my \@bg_$2;\n!g;
+		return $input;
+	}
+	sub comment_gobbler{#eats up comments from input
+		my $string = (shift @_);my $ans;
+		while ($string =~ m!([^#\n]*)(#[^\n]*\n)|([^#\n]*\n)!g){
+			if (defined $3){$ans = $ans . $3;}
+			else{$ans = $ans . $1 . "\n";}
+		}
+		return $ans;
+	}
+	sub get_index{#expects a ref to array names and a name whose index is to be found
+		my $input = shift (@_);my $name = shift (@_);my $i = 0;
+		while(defined $$input[$i][0]){
+			if($$input[$i][0] eq $name){return $i;}
+			$i = $i+1;
+		}
+		my $empty_val;
+		return $empty_val;#if no match return an empty variable;
+	}
+	sub convert{
+		my $a = ord('A');
+		my $num = (shift @_);
+		my $ans = '';
+		if ($num<0) {return ''};
+		if ($num==0){
+			return 'A';
+		}
+		else{
+			return convert(int ($num / 26) -1).chr(($num%26)+$a);
+		}
+		return $ans;
+	}
