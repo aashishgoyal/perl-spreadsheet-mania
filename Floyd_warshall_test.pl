50d444383b4e91a79a2a36d06f64a3bd7838e192
pujan003 <poojanmehta8994@gmail.com>
2014-06-26 21:07:59 +0530

    Create Floyd_warshall_test.pl
    A test file for part3.pl

---
+++ Floyd_warshall_test.pl
@@ -0,0 +1,61 @@
+#matric chain multiplication perl version.
+
+use Data::Dumper;
+
+	my @p; #dim:p[3][3]
+	my @p = ([0,1,2],
+			[5,0,3],
+			[1,2,0]);	
+	my $n = 3;
+
+	my @m;
+	#dim:m[4][3][3]
+	my @path;
+	#dim:path[4][3][3]
+
+	for (my $i = 0; $i < $n; $i++){
+		for (my $j = 0; $j < $n; $j++){
+			$m[0][$i][$j] = $p[$i][$j];
+			$path[0][$i][$j] = -1;
+		}
+	}
+
+
+	for(my $i=1; $i < $n+1 ; $i++){
+   	 for(my $j=0; $j < $n; $j++){
+        for(my $k=0; $k < $n; $k++){
+           $m[$i][$j][$k] = min($m[$i-1][$j][$k], $m[$i-1][$j][$i-1] + $m[$i-1][$i-1][$k] ) ;
+           $path[$i][$j][$k] = IF($m[$i-1][$j][$k] > ($m[$i-1][$j][$i-1] + $m[$i-1][$i-1][$k]),$i-1,$path[$i-1][$j][$k]);
+           
+        }
+    }
+}
+
+for(my $i=0; $i < $n+1 ; $i++){
+   	 for(my $j=0; $j < $n; $j++){
+        for(my $k=0; $k < $n; $k++){
+           print $m[$i][$j][$k]." ";
+           
+        }
+        print "\n";
+    }
+    print "\n\n ";
+}	
+
+	sub min {
+		my ($min, @vars) = @_;
+		for (@vars){
+			$min = $_ if $_ < $min;
+		}
+		return $min;
+	}
+
+	sub IF {
+		my ($c,$e1,$e2) = @_;
+		if ($c) {
+			return $e1;
+		}
+		else {
+			$e2;
+		}
+	}
